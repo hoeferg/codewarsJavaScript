99a8465dedca271e72c7fa7be3d7c7ebdbe924a5
Gayle Hoefer <gaylehoefer@gmail.com>
2023-01-02 13:31:46 -0700

    array

---
+++ Trainingjs/Array.js
@@ -0,0 +1,21 @@
+function getLength(arr) {
+    return arr.length
+}
+function getFirst(arr) {
+    return arr[0]
+}
+function getLast(arr) {
+    const arrayLast = arr[arr.length - 1];
+    return arrayLast
+}
+function pushElement(arr) {
+    var el = 1;
+    // arr.push(1);
+    arr.push(el);
+    return arr
+}
+
+function popElement(arr) {
+    arr.pop(1);
+    return arr
+}
