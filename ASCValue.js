f7ce43c0a0a34d8ea60830251c4a69c806fdccc7
Gayle Hoefer <gaylehoefer@gmail.com>
2023-01-02 11:16:05 -0700

    added ASCValue

---
+++ ASCValue.js
@@ -0,0 +1,6 @@
+//Write a function getChar which takes a number and returns the corresponding ASCII char for that value
+//A ASC value is a reference to a number or diget. When that key is pushed, that value is displayed
+function getChar(c) {
+    return String.fromCharCode(c);
+}
+
