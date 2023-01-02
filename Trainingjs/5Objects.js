a81efe0d52ba4353d800d22401ba4578a8a50403
Gayle Hoefer <gaylehoefer@gmail.com>
2023-01-02 13:49:38 -0700

    objects practice

---
+++ Trainingjs/5Objects.js
@@ -0,0 +1,15 @@
+// function animal(obj) {
+//     const animal =
+//         animal
+//     {
+//         animal.name = "dog"
+//         animal.leg = 4
+//         animal.color = "white"
+//     }
+//     return "This " + animal.color + " " + animal.name + " " + "has " + animal.leg + " " + "legs."
+// }
+
+function animal(obj) {
+
+    return "This " + obj.color + " " + obj.name + " " + "has " + obj.legs + " " + "legs."
+}
